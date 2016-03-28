0c4e543485cd43fe73c83fdf0861bf77710ef474
cyuseff <cr.yuseff@gmail.com>
2016-03-28 18:51:50 -0300

    Twitt component html

--- app_client/src/components/Twitt.jsx
+++ app_client/src/components/Twitt.jsx
@@ -2,10 +2,39 @@
 
 import React from 'react';
 
-export default function Twitt() {
+const months = ['jan', 'feb', 'mar', 'apr', 'may', 'jun', 'jul', 'aug', 'sep', 'oct', 'nov', 'dec'];
+
+function dateToString(dateStr) {
+  const date = new Date(dateStr);
+  return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
+}
+
+export default function Twitt({twitt}) {
   return(
     <li>
-      twiit 1
+      <small className="text-muted">reply</small>
+
+      <div className="media">
+        <div className="media-left">
+          <img className="media-object twitt-avatar" src={twitt.avatar} alt={twitt.name}/>
+        </div>
+        <div className="media-body">
+          <ul className="list-inline">
+            <li><a href={`https://twitter.com/${twitt.screenName}`} target="_blank">{twitt.name}</a></li>
+            <li className="text-muted">{`@${twitt.screenName}`}</li>
+            <li>{dateToString(twitt.createdAt)}</li>
+          </ul>
+
+          <h4>{twitt.text}</h4>
+
+          <ul className="list-inline">
+            <li><a href="https://twitter.com/intent/tweet?in_reply_to=463440424141459456">Reply</a></li>
+            <li><a href="https://twitter.com/intent/retweet?tweet_id=463440424141459456">Retweet</a></li>
+            <li><a href="https://twitter.com/intent/like?tweet_id=463440424141459456">Like</a></li>
+          </ul>
+
+        </div>
+      </div>
     </li>
   );
 };
