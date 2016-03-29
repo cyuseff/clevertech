265fdecabd7dab8dfff8a54874ed431b916675c4
cyuseff <cr.yuseff@gmail.com>
2016-03-29 00:15:20 -0300

    twitt user, hastags & link to html link

--- app_client/src/components/Twitt.jsx
+++ app_client/src/components/Twitt.jsx
@@ -9,6 +9,40 @@ function dateToString(dateStr) {
   return `${date.getDate()} ${months[date.getMonth()]} ${date.getFullYear()}`;
 }
 
+function getUsers(text, user_mentions) {
+  let reg;
+  user_mentions.forEach((item) => {
+    reg = new RegExp(`@${item.screen_name}`, 'i');
+    text = text.replace(reg, `<a href="https://twitter.com/${item.screen_name}" target="_blank">@${item.screen_name}</a>`);
+  });
+  return text;
+}
+
+function getHashTags(text, hashtags) {
+  let reg;
+  hashtags.forEach((item) => {
+    reg = new RegExp(`#${item.text}`, 'i');
+    text = text.replace(reg, `<a href="https://twitter.com/hashtag/${item.text}?src=hash" target="_blank">#${item.text}</a>`);
+  });
+  return text;
+}
+
+function getUrls(text, urls) {
+  let reg;
+  urls.forEach((item) => {
+    reg = new RegExp(item.url);
+    text = text.replace(reg, `<a href="${item.url}" target="_blank">${item.display_url}</a>`);
+  });
+  return text;
+}
+
+function createMarkup(text, entities) {
+  text = getUsers(text, entities.user_mentions);
+  text = getHashTags(text, entities.hashtags);
+  text = getUrls(text, entities.urls);
+  return {__html: `<span>${text}</span>`};
+};
+
 export default function Twitt({twitt}) {
   let retwitt;
   let reply;
@@ -47,7 +81,7 @@ export default function Twitt({twitt}) {
             <li>{dateToString(twitt.createdAt)}</li>
           </ul>
 
-          <h4>{twitt.text}</h4>
+          <h4 dangerouslySetInnerHTML={createMarkup(twitt.text, twitt.entities)}></h4>
 
           <ul className="list-inline twitt-actions">
             <li>
