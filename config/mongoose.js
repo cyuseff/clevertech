e494fee8cc87aa52c5d0f15a9f43808d2e861ef0
cyuseff <cr.yuseff@gmail.com>
2016-03-29 13:48:07 -0300

    heroku setup

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const mongoose = require('mongoose');
-const dbURI = process.env.MONOGO_URI || 'mongodb://localhost/clevertech';
+const dbURI = process.env.MONGO_URI || 'mongodb://localhost/clevertech';
 
 // Events
 mongoose.connection
